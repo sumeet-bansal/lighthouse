b63e6312e90f287d4325e093497c16371b3cf052
pkelaita <pierce@kelaita.com>
2017-07-10 16:53:21 -0700

    updated comparator to create a 2D array representing data in a CSV file

--- src/test/java/parserTester/ComparatorTester.java
+++ src/test/java/parserTester/ComparatorTester.java
@@ -4,10 +4,34 @@ import cachingLayer.Comparator;
 
 public class ComparatorTester {
 
+	/**
+	 * Test different outcomes of Comparator's methods
+	 * @param args
+	 */
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		Comparator c = new Comparator("C:/Test root");
+		
 		String c1 = "Test root/*/fabric1/*/ceph.conf";
 		String c2 = "Test root/*/fabric2/*/ceph.conf";
-		c.query(c1, c2);
+		String c3 = "Test root/*/fabric1/*/storm2.yaml";
+		String c4 = "Test root/*/fabric2/*/storm2.yaml";
+		String c5 = "Test root/*/fabric1/*/storm.server.properties";
+		String c6 = "Test root/*/fabric2/*/storm.server.properties";
+		String c7 = "Test root/*/fabric1/*/hosts";
+		String c8 = "Test root/*/fabric2/*/hosts";
+		
+		c.addQuery(c1, c2);
+		c.compare();
+		
+		System.out.println();
+		
+		String[][] table = c.getTable();
+		for (String[] arr : table) {
+			for (String str : arr) {
+				System.out.print(str + "     ");
+			}
+			System.out.println();
+		}
 	}
 }
